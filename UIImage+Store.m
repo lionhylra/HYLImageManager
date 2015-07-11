9c8e4fff77bfe83085796c75c6db8e1a72a842cf
HeYilei <leon@jeylabs.com>
2015-07-11 20:42:22 +1000

    edit licence

--- UIImage+Store.m
+++ UIImage+Store.m
@@ -3,6 +3,7 @@
 //The MIT License (MIT)
 //
 //Copyright (c) 2015 Yilei He
+//https://github.com/lionhylra/HYLImageManager
 //
 //Permission is hereby granted, free of charge, to any person obtaining a copy
 //of this software and associated documentation files (the "Software"), to deal
